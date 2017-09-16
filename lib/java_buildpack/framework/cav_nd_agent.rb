c932c272fc56f0218190f747babcc16546c6c665
netstorm <netstorm@netstorm-Vostro-2520.(none)>
2017-09-16 19:49:48 +0530

    apply ndHome

--- lib/java_buildpack/framework/cav_nd_agent.rb
+++ lib/java_buildpack/framework/cav_nd_agent.rb
@@ -33,7 +33,7 @@ module JavaBuildpack
       def release
         #credentials = @application.services.find_service(FILTER)['credentials']
         java_opts   = @droplet.java_opts
-        java_opts.add_javaagent(argument)
+        java_opts.add_javaagent(agent_args)
 
         #application_name java_opts, credentials
         #tier_name java_opts, credentials
