5b3b9c02bd347f22a9872d32c3e79f1ba5e538c5
netstorm <netstorm@netstorm-Vostro-2520.(none)>
2017-09-18 10:19:08 +0530

    apply ndHome

--- lib/java_buildpack/framework/cav_nd_agent.rb
+++ lib/java_buildpack/framework/cav_nd_agent.rb
@@ -51,7 +51,8 @@ module JavaBuildpack
 
       def agent_args
       	ndHome = agent_dir
-      	argument = '#{ndHome}lib/ndmain.jar=time,ndAgentJar=#{ndHome}lib/ndagent-with-dep.jar,ndHome=#{ndHome},tier=default,ndcHost=10.10.40.93,ndcPort=7892,BCILoggingMode=OUTPUT_STREAM'
+      	#argument = '#{ndHome}lib/ndmain.jar=time,ndAgentJar=#{ndHome}lib/ndagent-with-dep.jar,ndHome=#{ndHome},tier=default,ndcHost=10.10.40.93,ndcPort=7892,BCILoggingMode=OUTPUT_STREAM'
+      	argument = '#{ndHome}lib/ndmain.jar=time
       	argument
       end
 
